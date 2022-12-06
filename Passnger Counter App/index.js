4463300e49f566d853c4cd50b36f913988ceed2e
Christopher Guerrero <chrisgrr3@gmail.com>
2022-12-06 16:28:57 -0700

    More practice

--- Passnger Counter App/index.js
+++ Passnger Counter App/index.js
@@ -1,1 +1,18 @@
-document.getElementById("count-el").innerText = 5;
+// document.getElementById("count-el").innerText = 5;
+
+// let firstBatch = 5;
+// let secondBatch = 7;
+
+// let count = firstBatch + secondBatch;
+
+// console.log(count);
+
+let myAge = 24;
+
+let humanDogRatio = 7;
+
+let myDogAge = myAge * humanDogRatio;
+
+console.log(myDogAge);
+
+
