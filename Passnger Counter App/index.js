55ec83e9c587e03301be8c1a7b368424463f0794
Christopher Guerrero <chrisgrr3@gmail.com>
2022-12-07 16:29:21 -0700

    Update index.js
    Removed unused operations (were commented out)

--- Passnger Counter App/index.js
+++ Passnger Counter App/index.js
@@ -1,33 +1,3 @@
-// document.getElementById("count-el").innerText = 5;
-
-// let firstBatch = 5;
-// let secondBatch = 7;
-
-// let count = firstBatch + secondBatch;
-
-// console.log(count);
-
-// let myAge = 24;
-
-// let humanDogRatio = 7;
-
-// let myDogAge = myAge * humanDogRatio;
-
-// console.log(myDogAge);
-
-
-// let bonusPoints = 50;
-// console.log(bonusPoints);
-
-// bonusPoints = bonusPoints + 50;
-// console.log(bonusPoints);
-
-// bonusPoints = bonusPoints - 75;
-// console.log(bonusPoints);
-
-// bonusPoints = bonusPoints + 45;
-// console.log(bonusPoints);
-
 let count = 0;
 
 function increment() {
