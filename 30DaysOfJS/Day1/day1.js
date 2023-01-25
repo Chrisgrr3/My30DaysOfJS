63b9f8427bafce59fb95326e8feeff46a065fa12
Christopher Guerrero <chrisgrr3@gmail.com>
2023-01-25 01:14:46 -0700

    Numbered each comment, also finished first problem

--- 30DaysOfJS/Day1/day1.js
+++ 30DaysOfJS/Day1/day1.js
@@ -1,19 +1,21 @@
-// Write a single line comment which says, comments can make code readable
+// 1. Write a single line comment which says, comments can make code readable
 
-// Write another single comment which says, Welcome to 30DaysOfJavaScript
+// Comments can make code readable.
 
-// Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+// 2. Write another single comment which says, Welcome to 30DaysOfJavaScript
 
-// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+// 3. Write a multiline comment which says, comments can make code readable, easy to reuse and informative
 
-// Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+// 4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
 
-// Declare four variables without assigning values
+// 5. Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
 
-// Declare four variables with assigned values
+// 6. Declare four variables without assigning values
 
-// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+// 7. Declare four variables with assigned values
 
-// Declare variables to store your first name, last name, marital status, country and age in a single line
+// 8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
 
-// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+// 9. Declare variables to store your first name, last name, marital status, country and age in a single line
+
+// 10. Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
