51e838e45ae2bda8028eb2a09a90039f72157172
Christopher Guerrero <chrisgrr3@gmail.com>
2023-01-28 00:34:18 -0700

    Calculated the slope, x intercept, and y intercept in question 5

--- 30DaysOfJS/Day3/day3_level_2.js
+++ 30DaysOfJS/Day3/day3_level_2.js
@@ -68,6 +68,9 @@ function circle () {
 
 
 // 5. Calculate the slope, x-intercept and y-intercept of y = 2x -2
+console.log(`The slope is 2`)
+console.log('The x-intercept is 1')
+console.log('The y-intercept is -2')
 
 // 6. Slope is m = (y2-y1)/(x2-x1). Find the slope between point (2, 2) and point(6,10)
 
