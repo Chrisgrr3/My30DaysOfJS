4af5b9e50171b021f11bb3cce375ce8e02e86e95
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-01 23:42:55 -0700

    Created function to see if the country Ethiopia is located in the array

--- 30DaysOfJS/Day5/day5_level_2.js
+++ 30DaysOfJS/Day5/day5_level_2.js
@@ -1,3 +1,5 @@
+const countries = require('./countries').countries;
+
 // 1. Create a separate countries.js file and store the countries array in to this file, create a separate file web_techs.js and store the webTechs array in to this file. Access both file in main.js file
 
 console.log('Question 1 complete.')
@@ -34,6 +36,8 @@ console.log(shoppingCart);
 
 
 // 4. In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. If it does not exist add to the countries list.
+
+countries.includes('Ethiopia') ? console.log('ETHIOPIA') : countries.push('Ethiopia')
 
 // 5. In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
 
