a478ac6750c6770d35797ca387923fa47e20a73c
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-01 23:34:29 -0700

    Created a shopping cart array and modified values

--- 30DaysOfJS/Day5/day5_level_2.js
+++ 30DaysOfJS/Day5/day5_level_2.js
@@ -13,12 +13,25 @@ console.log(text.length)
 
 // 3. In the following shopping cart add, remove, edit items
 
-    // const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+if (! shoppingCart.includes('Meat')) {
+    shoppingCart.unshift('Meat')
+}
+console.log(shoppingCart)
+if (! shoppingCart.includes('Sugar')) {
+    shoppingCart.push('Sugar')
+}
+console.log(shoppingCart)
+const allergicToHoney = false;
 
-        // add 'Meat' in the beginning of your shopping cart if it has not been already added
-        // add Sugar at the end of you shopping cart if it has not been already added
-        // remove 'Honey' if you are allergic to honey
-        // modify Tea to 'Green Tea'
+if(allergicToHoney) {
+    shoppingCart.splice(shoppingCart.indexOf('Honey'), 1);
+}
+console.log(shoppingCart)
+
+shoppingCart[shoppingCart.indexOf('Tea')] = 'Green Tea'
+console.log(shoppingCart);
+
 
 // 4. In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. If it does not exist add to the countries list.
 
