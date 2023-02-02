395a3ad4b8f92c185a58761e3a4cfd1bf22d4bd7
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-01 23:47:41 -0700

    Checked if the Sass technology exists in the web_techs array

--- 30DaysOfJS/Day5/day5_level_2.js
+++ 30DaysOfJS/Day5/day5_level_2.js
@@ -1,4 +1,5 @@
 const countries = require('./countries').countries;
+const web_techs = require('./webTechs').webTechs;
 
 // 1. Create a separate countries.js file and store the countries array in to this file, create a separate file web_techs.js and store the webTechs array in to this file. Access both file in main.js file
 
@@ -40,6 +41,8 @@ console.log(shoppingCart);
 countries.includes('Ethiopia') ? console.log('ETHIOPIA') : countries.push('Ethiopia')
 
 // 5. In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
+
+web_techs.includes('Sass') ? console.log('Sass is a CSS preprocess') : web_techs.push('Sass') && console.log(web_techs);
 
 // 6. Concatenate the following two variables and store it in a fullStack variable.
 
