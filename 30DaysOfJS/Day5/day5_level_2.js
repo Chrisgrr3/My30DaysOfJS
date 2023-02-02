377ab218e520476a3df4017985160d8ef42cbca4
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-01 23:51:52 -0700

    Concatenated two arrays and stored the resulting array into a separate variable.

--- 30DaysOfJS/Day5/day5_level_2.js
+++ 30DaysOfJS/Day5/day5_level_2.js
@@ -46,8 +46,7 @@ web_techs.includes('Sass') ? console.log('Sass is a CSS preprocess') : web_techs
 
 // 6. Concatenate the following two variables and store it in a fullStack variable.
 
-    // const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
-    // const backEnd = ['Node','Express', 'MongoDB']
-
-    // console.log(fullStack)
-    // ["HTML", "CSS", "JS", "React", "Redux", "Node", "Express", "MongoDB"]
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux'];
+const backEnd = ['Node','Express', 'MongoDB'];
+const fullStack = frontEnd.concat(backEnd);
+console.log(fullStack)
