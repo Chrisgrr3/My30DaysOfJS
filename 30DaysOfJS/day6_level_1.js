ba88156a634e71eccb30750d4fc8f7316c3d04a5
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-03 22:33:42 -0700

    Counted up to 10 using a for loop, while loop, and do while loop

--- 30DaysOfJS/day6_level_1.js
+++ 30DaysOfJS/day6_level_1.js
@@ -25,8 +25,26 @@ const countries = [
   const mernStack = ['MongoDB', 'Express', 'React', 'Node']
 
 //   1. Iterate 0 to 10 using for loop, do the same using while and do while loop
-  
 
+for(let i = 0; i <= 10; i++) {
+    // console.log(i)
+}
+
+// or
+
+let j = 0
+while(j <= 10) {
+    // console.log(j)
+    j++
+}
+
+// or
+
+let k = 0
+do {
+    // console.log(k)
+    k++
+} while (k <= 10) 
 
 //   2. Iterate 10 to 0 using for loop, do the same using while and do while loop
   
