c73c58ffe6a8b275599cc5559e2d2aef56491463
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-05 22:04:24 -0700

    Created function to determine the sum of numbers from 0 to 100

--- 30DaysOfJS/day6_level_1.js
+++ 30DaysOfJS/day6_level_1.js
@@ -187,7 +187,14 @@ function prime() {
 
     //   The sum of all numbers from 0 to 100 is 5050.
 
-
+function hundredSum() {
+    let sum = 0;
+    for(let i = 0; i <= 100; i++) {
+        sum += i;
+    }
+    console.log(sum)
+}
+// hundredSum()
 
 //   11. Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds.
   
