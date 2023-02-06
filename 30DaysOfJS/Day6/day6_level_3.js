f92432a8c449450b351a83fcf92f2c0dd4dcc43d
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-06 15:56:36 -0700

    Created a copy of the countries array

--- 30DaysOfJS/Day6/day6_level_3.js
+++ 30DaysOfJS/Day6/day6_level_3.js
@@ -1,5 +1,18 @@
 // 1. Copy countries array(Avoid mutation)
 
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ]
 
 // 2. Arrays are mutable. Create a copy of array which does not modify the original. Sort the copied array and store in a variable sortedCountries
 
