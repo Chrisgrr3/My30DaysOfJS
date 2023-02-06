e9738e48b053ab91f7ce9175b5e11516a0aa17bf
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-06 15:44:42 -0700

    Constructed for loop that iterates through an array of arrays and prints out each element in uppercase letters

--- 30DaysOfJS/Day6/day6_level_2.js
+++ 30DaysOfJS/Day6/day6_level_2.js
@@ -265,3 +265,13 @@ console.log(fruits)
         //   NODE
         //   EXPRESS
         //   MONGODB
+
+const fullStack = [
+    ['HTML', 'CSS', 'JS', 'React'],
+    ['Node', 'Express', 'MongoDB']
+]
+for(let i = 0; i < fullStack.length; i++) {
+    for(let j = 0; j < fullStack[i].length; j++) {
+        console.log(fullStack[i][j].toUpperCase())
+    }
+}
