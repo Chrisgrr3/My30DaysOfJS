ac7848cc3566f50ebf2b1566706c1461c23495d9
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-06 15:11:34 -0700

    Created an array that includes only elements from the countries array that contain the word 'land'.

--- 30DaysOfJS/Day6/day6_level_2.js
+++ 30DaysOfJS/Day6/day6_level_2.js
@@ -139,7 +139,17 @@ for(let i = 0; i < countries.length; i++) {
 
     // ['Finland','Ireland', 'Iceland']
 
-
+const landArray = []
+for(let i = 0; i < countries.length; i++) {
+    if(countries[i].includes('land')) {
+        landArray.push(countries[i])
+    }
+}
+if (landArray.length === 0) {
+    console.log('All these countries are without land')
+} else {
+    console.log(landArray)
+}
 
 // 8. In above countries array, check if there is a country or countries end with a substring 'ia'. If there are countries end with, print it as array. If there is no country containing the word 'ai', print 'These are countries ends without ia'.
 
