7f7d8d89bfb55b2d4788c19e2b80d63279687e60
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-08 23:51:33 -0700

    Created function to convert Celsius to Fahrenheit

--- 30DaysOfJS/Day7/day7_level_1.js
+++ 30DaysOfJS/Day7/day7_level_1.js
@@ -95,7 +95,9 @@ function weight(mass, gravity) {
 
 // 12. Temperature in oC can be converted to oF using this formula: oF = (oC x 9/5) + 32. Write a function which convert oC to oF convertCelsiusToFahrenheit.
 
-
+function convertCelsiusToFahrenheit(oC) {
+    const oF = (oC * (9/5) + 32);
+}
 
 
 // 13. Body mass index(BMI) is calculated as follows: bmi = weight in Kg / (height x height) in m2. Write a function which calculates bmi. BMI is used to broadly define different weight groups in adults 20 years old or older.Check if a person is underweight, normal, overweight or obese based the information given below.
