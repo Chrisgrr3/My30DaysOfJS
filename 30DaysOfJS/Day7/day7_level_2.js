cfa2f66b70fa459a1fa9ecb9e454cdcad5a78bd0
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-10 23:52:29 -0700

    Created function to swap two values

--- 30DaysOfJS/Day7/day7_level_2.js
+++ 30DaysOfJS/Day7/day7_level_2.js
@@ -70,6 +70,13 @@ showDateTime()
     // swapValues(3, 4) // x => 4, y=>3
     // swapValues(4, 5) // x = 5, y = 4
 
+function swapValues(x, y) {
+    let temp = x;
+    x = y;
+    y = temp;
+    console.log(x, y)
+}
+swapValues(1, 2)
 
 
 // 6. Declare a function name reverseArray. It takes array as a parameter and it returns the reverse of the array (don't use method).
