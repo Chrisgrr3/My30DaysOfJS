fb9f3fa99b6033c8357cfb1da3bf280f050f62c4
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-15 11:51:13 -0700

    Added properties and a method to dog object

--- 30DaysOfJS/Day8/day8_level_1.js
+++ 30DaysOfJS/Day8/day8_level_1.js
@@ -13,6 +13,14 @@ console.log(dog)
 
 // 3. Add name, legs, color, age and bark properties for the dog object. The bark property is a method which return woof woof
 
+dog.name = 'daisy'
+dog.legs = 4
+dog.color = 'white'
+dog.bark = function() {
+    return 'woof woof'
+}
+console.log(dog)
+console.log(dog.bark())
 
 
 // 4. Get name, legs, color, age and bark value from the dog object
