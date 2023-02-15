e01fbbfb977a2e646306f20314acb53d90f14a68
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-15 11:52:52 -0700

    Completed question 4

--- 30DaysOfJS/Day8/day8_level_1.js
+++ 30DaysOfJS/Day8/day8_level_1.js
@@ -16,6 +16,7 @@ console.log(dog)
 dog.name = 'daisy'
 dog.legs = 4
 dog.color = 'white'
+dog.age = 12
 dog.bark = function() {
     return 'woof woof'
 }
@@ -25,6 +26,11 @@ console.log(dog.bark())
 
 // 4. Get name, legs, color, age and bark value from the dog object
 
+console.log(dog.name)
+console.log(dog.legs)
+console.log(dog.color)
+console.log(dog.age)
+console.log(dog.bark())
 
 
 // 5. Set new properties the dog object: breed, getDogInfo
