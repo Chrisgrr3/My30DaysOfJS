16a86c86a2774547c5887d91aedbd646f07bc535
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-20 16:51:03 -0700

    Destructured users keys using a for loop

--- 30DaysOfJS/Day11/day11_level_2.js
+++ 30DaysOfJS/Day11/day11_level_2.js
@@ -1,8 +1,10 @@
-const users = require('./day11_level_1')
+const { users } = require('./day11_level_1')
 
 // 1. Iterate through the users array and get all the keys of the object using destructuring
 
-
+for(const {name, scores, skills, age} of users) {
+    console.log(`Name: ${name} Scores: ${scores} Skills: ${skills} Age: ${age}`)
+}
 
 
 // 2. Find the persons who have less than two skills
