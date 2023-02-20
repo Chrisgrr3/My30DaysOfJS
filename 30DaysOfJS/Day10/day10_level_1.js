f462582a895d13cc6c4b30f243d560a867a8a656
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-19 21:03:03 -0700

    Formatting

--- 30DaysOfJS/Day10/day10_level_1.js
+++ 30DaysOfJS/Day10/day10_level_1.js
@@ -1,9 +1,20 @@
-// const a = [4, 5, 8, 9]
-// const b = [3, 4, 5, 7]
-// const countries = ['Finland', 'Sweden', 'Norway']
-// create an empty set
-// Create a set containing 0 to 10 using loop
-// Remove an element from a set
-// Clear a set
-// Create a set of 5 string elements from array
-// Create a map of countries and number of characters of a country
+const a = [4, 5, 8, 9]
+const b = [3, 4, 5, 7]
+const countries = ['Finland', 'Sweden', 'Norway']
+
+// 1. create an empty set
+
+
+// 2. Create a set containing 0 to 10 using loop
+
+
+// 3. Remove an element from a set
+
+
+// 4. Clear a set
+
+
+// 5. Create a set of 5 string elements from array
+
+
+// 6. Create a map of countries and number of characters of a country
