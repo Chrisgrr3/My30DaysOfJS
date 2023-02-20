f8ddc5401e9ec5582fd744a80e86b11c649fc857
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-19 23:00:37 -0700

    Found the union for sets a and b

--- 30DaysOfJS/Day10/day10_level_2.js
+++ 30DaysOfJS/Day10/day10_level_2.js
@@ -1,8 +1,12 @@
 const a = [4, 5, 8, 9]
 const b = [3, 4, 5, 7]
 
+const A = new Set(a)
+const B = new Set(b)
 // 1. Find a union b
 
+const c = a.filter((num) => B.has(num))
+console.log(c)
 
 // 2. Find a intersection b
 
