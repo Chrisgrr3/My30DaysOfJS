bb8e716fc219c7819c47378ec0751aaa35164a25
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-20 19:39:07 -0700

    Destructured countries object for its name, capital, population, and languages variable.

--- 30DaysOfJS/Day11/day11_level_3.js
+++ 30DaysOfJS/Day11/day11_level_3.js
@@ -1,9 +1,11 @@
-const { countries } = require('./day11_level_1')
+const { countries } = require('../Day8/countries')
 
 console.log(countries)
 // 1. Destructure the countries object print name, capital, population and languages of all countries
 
-
+for(const {name, capital, population, languages} of countries) {
+    console.log(name, capital, population, languages)
+}
 
 
 // 2. A junior developer structure student name, skills and score in array of arrays which may not easy to read. Destructure the following array name to name, skills array to skills, scores array to scores, JavaScript score to jsScore and React score to reactScore variable in one line.
