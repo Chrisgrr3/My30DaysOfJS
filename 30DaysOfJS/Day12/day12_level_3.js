1874e8963f0253e9e2b7d9a11bee0f206b8a826e
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-21 14:15:50 -0700

    Began function for question 2 and modified sentence variable

--- 30DaysOfJS/Day12/day12_level_3.js
+++ 30DaysOfJS/Day12/day12_level_3.js
@@ -3,10 +3,12 @@
 let sentence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; && mo@re rewarding th%an educa@ting &and& @emp%o@wering peo@ple. ;I find tea@ching m%o@re interesting tha@n other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`
 function cleanText(s) {
     s = s.match(/[^%@&#$!;]/gi).join('')
-    console.log(s)
+    return s
 }
-cleanText(sentence)
+sentence = cleanText(sentence)
+console.log(sentence)
 
 
 // 2. Write a function which find the three most frequent words. 
 
+function threeMostFrequent
