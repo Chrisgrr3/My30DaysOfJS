8ee5b1f7561c46d80316dc313cb7c8511dfacf0a
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-21 14:12:09 -0700

    Created function that cleans a sentence using a regular expression to remove all unwanted characters

--- 30DaysOfJS/Day12/day12_level_3.js
+++ 30DaysOfJS/Day12/day12_level_3.js
@@ -1,7 +1,12 @@
-// 1. Write a function which cleans text. Clean the following text. After cleaning, count three most frequent words in the string.
+// 1. Write a function which cleans text. Clean the following text.
 
-let sentence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`
+let sentence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; && mo@re rewarding th%an educa@ting &and& @emp%o@wering peo@ple. ;I find tea@ching m%o@re interesting tha@n other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`
+function cleanText(s) {
+    s = s.match(/[^%@&#$!;]/gi).join('')
+    console.log(s)
+}
+cleanText(sentence)
 
 
-// 2. Write a function which find the most frequent words. After cleaning, count three most frequent words in the string.
+// 2. Write a function which find the three most frequent words. 
 
