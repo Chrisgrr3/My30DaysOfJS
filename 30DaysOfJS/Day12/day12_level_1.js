18a3c583fd512ebb8b8dab121e512516936ab7ed
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-21 11:52:25 -0700

    Used RegExp to find the monthly income in question 1

--- 30DaysOfJS/Day12/day12_level_1.js
+++ 30DaysOfJS/Day12/day12_level_1.js
@@ -1,6 +1,13 @@
 // 1. Calculate the total annual income of the person from the following text. ‘He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month.’
-
-
+const str = 'He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month.'
+function totalAnnualIncome() {
+    const monthlyPattern = /\d+/
+    const monthlyMatch = str.match(monthlyPattern)
+    const monthly = monthlyMatch[0]
+    
+    
+}
+totalAnnualIncome()
 
 
 // 2. The position of some particles on the horizontal x-axis -12, -4, -3 and -1 in the negative direction, 0 at origin, 4 and 8 in the positive direction. Extract these numbers and find the distance between the two furthest particles.
