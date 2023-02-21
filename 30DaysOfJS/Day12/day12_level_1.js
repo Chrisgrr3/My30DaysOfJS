6c6be07bb96af05e4f8345cfcbf8f70f9059b8fb
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-21 12:17:17 -0700

    Completed question 1

--- 30DaysOfJS/Day12/day12_level_1.js
+++ 30DaysOfJS/Day12/day12_level_1.js
@@ -1,11 +1,13 @@
 // 1. Calculate the total annual income of the person from the following text. ‘He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month.’
 const str = 'He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month.'
 function totalAnnualIncome() {
-    const monthlyPattern = /\d+/
-    const monthlyMatch = str.match(monthlyPattern)
-    const monthly = monthlyMatch[0]
-    
-    
+    let [monthly, bonus, courses] = str.match(/\d{1,}/g)
+    monthly = parseInt(monthly)
+    bonus = parseInt(bonus)
+    courses = parseInt(courses)
+    const annualIncome = 12 * monthly + bonus + 12 * courses
+    console.log(annualIncome)
+    return annualIncome
 }
 totalAnnualIncome()
 
