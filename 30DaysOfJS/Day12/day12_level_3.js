61bc54a1959a5b62f135f6d58ffb79f8ef419d5a
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-21 14:39:03 -0700

    Created function for finding the three most frequent words using regular expression

--- 30DaysOfJS/Day12/day12_level_3.js
+++ 30DaysOfJS/Day12/day12_level_3.js
@@ -11,4 +11,26 @@ console.log(sentence)
 
 // 2. Write a function which find the three most frequent words. 
 
-function threeMostFrequent
+function threeMostFrequent(s) {
+    const words = s.match(/\w{1,}/gi)
+    const wordSet = new Set(words)
+    const result = []
+    const topThree = []
+    let count;
+    let regEx;
+    for(let word of wordSet) {
+        regEx = new RegExp(`${word}+`, 'g')
+        count = s.match(regEx).length
+        result.push({word: word, count: count})
+    }
+    
+    result.sort((a, b) => {
+        if(a.count > b.count) return -1
+        if(a.count < b.count) return 1
+    })
+    for(let i = 0; i < 3; i++) {
+        topThree.push(result[i])
+    }
+    return topThree
+}
+console.log(threeMostFrequent(sentence))
