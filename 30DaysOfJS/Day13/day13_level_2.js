f923b3f2172cf4df8416df054c2c71f09d550b4c
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-22 10:38:39 -0700

    Created warning message using console.warn()

--- 30DaysOfJS/Day13/day13_level_2.js
+++ 30DaysOfJS/Day13/day13_level_2.js
@@ -5,7 +5,7 @@ console.assert(10 > 2*10, '10 is not greater than 20.')
 
 // 2. Write a warning message using console.warn()
 
-
+console.warn('Usage of the variable \'name\' has been deprecated.')
 
 
 // 3. Write an error message using console.error()
