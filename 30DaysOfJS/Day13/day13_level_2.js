2b6ed28b6436a815737bb0d2b6722f6ff6c61c03
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-22 10:34:50 -0700

    Commented out all fields

--- 30DaysOfJS/Day13/day13_level_2.js
+++ 30DaysOfJS/Day13/day13_level_2.js
@@ -1,3 +1,3 @@
-10 > 2 * 10 use console.assert()
-Write a warning message using console.warn()
-Write an error message using console.error()
+// 10 > 2 * 10 use console.assert()
+// Write a warning message using console.warn()
+// Write an error message using console.error()
