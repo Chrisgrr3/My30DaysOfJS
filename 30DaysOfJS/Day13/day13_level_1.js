6c76c761e48fc631a15a1bccae8226cd93199917
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-22 10:27:05 -0700

    Imported necessary countries array

--- 30DaysOfJS/Day13/day13_level_1.js
+++ 30DaysOfJS/Day13/day13_level_1.js
@@ -1,3 +1,8 @@
+const countries = [
+    ['Finland', 'Helsinki'],
+    ['Sweden', 'Stockholm'],
+    ['Norway', 'Oslo']
+  ]
 // Display the countries array as a table
 // Display the countries object as a table
 // Use console.group() to group logs
