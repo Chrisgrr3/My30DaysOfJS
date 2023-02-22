6683f22105f846de699307777ce5d5e5777bb8f8
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-22 10:36:42 -0700

    Formatting

--- 30DaysOfJS/Day13/day13_level_2.js
+++ 30DaysOfJS/Day13/day13_level_2.js
@@ -1,3 +1,12 @@
-// 10 > 2 * 10 use console.assert()
-// Write a warning message using console.warn()
-// Write an error message using console.error()
+// 1. 10 > 2 * 10 use console.assert()
+
+
+
+
+// 2. Write a warning message using console.warn()
+
+
+
+
+// 3. Write an error message using console.error()
+
