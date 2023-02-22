952dd666320f88c9b2f572e8df794ce37db62cf1
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-22 14:21:18 -0700

    Created Animal class along with different methods

--- 30DaysOfJS/Day 15 - Classes/day15_level_1.js
+++ 30DaysOfJS/Day 15 - Classes/day15_level_1.js
@@ -1,6 +1,38 @@
 // 1. Create an Animal class. The class will have name, age, color, legs properties and create different methods
 
+class Animal {
+    constructor(name, age, color, legs) {
+        this.name = name
+        this.age = age
+        this.color = color
+        this.legs = legs
+    }
 
+    get getName() {
+        return this.name
+    }
+    get getAge() {
+        return this.age
+    }
+    get getColor() {
+        return this.color
+    }
+    get getLegs() {
+        return this.legs
+    }
+    set setName(name) {
+        this.name = name
+    }
+    set setAge(age) {
+        this.age = age
+    }
+    set setColor(color) {
+        this.color = color
+    }
+    set setLegs(legs) {
+        this.legs = legs
+    }
+}
 
 
 // 2. Create a Dog and Cat child class from the Animal Class.
