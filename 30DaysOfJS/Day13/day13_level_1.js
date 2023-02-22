b46c6d2ca33df16e4bec240a5eb3c6abdc864143
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-22 10:27:30 -0700

    Formatting

--- 30DaysOfJS/Day13/day13_level_1.js
+++ 30DaysOfJS/Day13/day13_level_1.js
@@ -3,6 +3,16 @@ const countries = [
     ['Sweden', 'Stockholm'],
     ['Norway', 'Oslo']
   ]
-// Display the countries array as a table
-// Display the countries object as a table
+
+// 1. Display the countries array as a table
+
+
+
+
+// 2. Display the countries object as a table
+
+
+
+
 // Use console.group() to group logs
+
