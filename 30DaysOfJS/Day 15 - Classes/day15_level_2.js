5a9a9718e1e9858fbcbae14f1e4d0040c13c61e2
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-23 00:30:50 -0700

    Overwrote  whatAmI() method

--- 30DaysOfJS/Day 15 - Classes/day15_level_2.js
+++ 30DaysOfJS/Day 15 - Classes/day15_level_2.js
@@ -2,3 +2,15 @@ const { Animal } = require('./day15_level_1')
 
 // 1. Override the method you create in Animal class
 
+class Snake extends Animal{
+    constructor(name, age, color, legs = 0) {
+        super(name, age, color, legs) 
+    }
+
+    whatAmI() {
+        console.log(`I'M A SNAAAAKE!`)
+    }
+}
+
+const mySnake = new Snake('Slytherin', 300, 'Green')
+mySnake.whatAmI()
