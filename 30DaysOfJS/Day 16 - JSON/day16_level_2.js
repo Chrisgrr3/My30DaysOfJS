d256dc92d9a53371317d54b378db8f4548fb501f
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-24 16:16:29 -0700

    Stringified student object

--- 30DaysOfJS/Day 16 - JSON/day16_level_2.js
+++ 30DaysOfJS/Day 16 - JSON/day16_level_2.js
@@ -2,3 +2,5 @@ let { student } = require('./day16_level_1')
 
 // 1. Stringify the students object with only firstName, lastName and skills properties
 
+student = JSON.stringify(student,['firstName', 'lastName', 'skills'])
+console.log(student)
