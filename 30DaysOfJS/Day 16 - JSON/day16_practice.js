97080196adb52861915adb40afd5564c99ee8664
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-24 14:01:02 -0700

    Created practice file with JSON object example

---
+++ 30DaysOfJS/Day 16 - JSON/day16_practice.js
@@ -0,0 +1,22 @@
+{
+    "users":[
+        {
+            "firstName":"Chris",
+            "lastName":"Guerrero",
+            "age":250,
+            "email":"chris@chris.com"
+        },
+        {
+            "firstName":"Albus",
+            "lastName":"Dumbledore",
+            "age":70,
+            "email":"dumbledore@hogwarts.com"
+        },
+        {
+            "firstName": "Harry",
+            "lastName": "Potter",
+            "age": 19,
+            "email": "potter@hogwarts.com"
+        }
+    ]
+}
