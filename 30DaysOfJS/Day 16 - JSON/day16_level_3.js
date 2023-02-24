fc39ce37d90182b92af74ab60e708b39f3061d16
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-24 16:37:14 -0700

    Found the user with the most skills and logged it to the console.

--- 30DaysOfJS/Day 16 - JSON/day16_level_3.js
+++ 30DaysOfJS/Day 16 - JSON/day16_level_3.js
@@ -3,8 +3,21 @@ let { txt } = require('./day16_level_1')
 // 1. Parse the txt JSON to object.
 
 txt = JSON.parse(txt)
-console.log(txt)
+// console.log(txt)
 
 
 // 2. Find the user who has many skills from the variable stored in txt.
 
+const keys = Object.keys(txt)
+// console.log(keys)
+let mostSkills = 0;
+let userKey = '';
+
+for(let key of keys) {
+    if(txt[key].skills.length > mostSkills) {
+        mostSkills = txt[key].skills.length;
+        userKey = key
+    }
+}
+
+console.log(`The user with the most skills is ${userKey}, who has ${mostSkills} skills.`)
