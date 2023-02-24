6fdebc9f769f79e4c8583304d277775ef33e6507
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-24 15:24:36 -0700

    Used callback function to modify parsed JSON

--- 30DaysOfJS/Day 16 - JSON/day16_practice.js
+++ 30DaysOfJS/Day 16 - JSON/day16_practice.js
@@ -1,4 +1,4 @@
-const usersText = `{
+const userText = `{
     "users":[
       {
         "firstName":"Asabeneh",
@@ -21,5 +21,38 @@ const usersText = `{
     ]
 }`
 
-const userObj = JSON.parse(usersText, undefined, 4)
+const userObj = JSON.parse(userText, undefined, 4)
 console.log(userObj)
+
+const usersText = `{
+    "users":[
+      {
+        "firstName":"Asabeneh",
+        "lastName":"Yetayeh",
+        "age":250,
+        "email":"asab@asb.com"
+      },
+      {
+        "firstName":"Alex",
+        "lastName":"James",
+        "age":25,
+        "email":"alex@alex.com"
+      },
+      {
+      "firstName":"Lidiya",
+      "lastName":"Tekle",
+      "age":28,
+      "email":"lidiya@lidiya.com"
+      }
+    ]
+    }`
+
+    // Below, we are modifying every value that is a string, but only if that value doesn't have the key 'email'
+const usersObj = JSON.parse(usersText, (key, value) => {
+    let newValue =
+        typeof value == 'string' && key != 'email' ? value.toUpperCase() : value
+    return newValue
+})
+
+
+console.log(usersObj)
