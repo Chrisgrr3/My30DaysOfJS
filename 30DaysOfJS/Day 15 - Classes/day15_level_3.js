c69418271155ead0cecb227b0eb61e79c0dda1d5
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-23 19:34:01 -0700

    Created PersonAccount class

--- 30DaysOfJS/Day 15 - Classes/day15_level_3.js
+++ 30DaysOfJS/Day 15 - Classes/day15_level_3.js
@@ -122,3 +122,6 @@ console.log(statistics.freqDist())
 
 // 2.Create a class called PersonAccount. It has firstname, lastname, incomes, expenses properties and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods. Incomes is a set of incomes and its description and expenses is also a set of expenses and its description.
 
+class PersonAccount {
+    
+}
