61dea90a4643c6e9aa2942b8adb1cf6ff31a3a85
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-24 16:19:59 -0700

    Parsed txt JSON object into JS Object

--- 30DaysOfJS/Day 16 - JSON/day16_level_3.js
+++ 30DaysOfJS/Day 16 - JSON/day16_level_3.js
@@ -2,7 +2,8 @@ let { txt } = require('./day16_level_1')
 
 // 1. Parse the txt JSON to object.
 
-
+txt = JSON.parse(txt)
+console.log(txt)
 
 
 // 2. Find the user who has many skills from the variable stored in txt.
