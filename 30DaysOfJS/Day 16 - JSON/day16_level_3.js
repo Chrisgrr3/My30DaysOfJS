d9a984ac1158f2e7fe5cba394b71479bf8bb88a0
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-24 16:17:29 -0700

    Formatting

--- 30DaysOfJS/Day 16 - JSON/day16_level_3.js
+++ 30DaysOfJS/Day 16 - JSON/day16_level_3.js
@@ -1,2 +1,7 @@
-Parse the txt JSON to object.
-Find the user who has many skills from the variable stored in txt.
+// 1. Parse the txt JSON to object.
+
+
+
+
+// 2. Find the user who has many skills from the variable stored in txt.
+
