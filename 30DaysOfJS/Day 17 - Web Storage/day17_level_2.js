16b7b12bc51292362b8f5aed420eb72daedb475f
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-25 11:22:46 -0700

    Defined student object

--- 30DaysOfJS/Day 17 - Web Storage/day17_level_2.js
+++ 30DaysOfJS/Day 17 - Web Storage/day17_level_2.js
@@ -1,2 +1,19 @@
 // 1. Create a student object. The student object will have first name, last name, age, skills, country, enrolled keys and values for the keys. Store the student object in your browser localStorage.
 
+const student = {
+    firstName: 'Chris',
+    lastName: 'Guerrero',
+    age: 290,
+    skills: [
+        'HTML',
+        'CSS',
+        'JavaScript',
+        'React'
+    ],
+    country: 'USA',
+    enrolled: [
+        'CS50',
+        'MATH404',
+        'ENG200'
+    ]
+}
