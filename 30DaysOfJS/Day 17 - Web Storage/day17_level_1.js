ec1a3b6a97dd72efae213e9e62822cd81c7f5978
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-25 11:11:25 -0700

    Wrote explanation as to why code won't run

--- 30DaysOfJS/Day 17 - Web Storage/day17_level_1.js
+++ 30DaysOfJS/Day 17 - Web Storage/day17_level_1.js
@@ -5,3 +5,5 @@ localStorage.setItem('lastName', 'Guerrero')
 localStorage.setItem('age', 24)
 localStorage.setItem('country', 'USA')
 localStorage.setItem('city', 'Phoenix')
+
+// These functions won't run unless they are run in a browser, since localStorage is part of the window object.
