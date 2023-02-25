8aea888b4728813f455db874fbe9e1447c7b7a9f
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-25 11:08:47 -0700

    Stored first name, last name, age, coutnry, and city into localStorage

--- 30DaysOfJS/Day 17 - Web Storage/day17_level_1.js
+++ 30DaysOfJS/Day 17 - Web Storage/day17_level_1.js
@@ -1,2 +1,7 @@
 // 1. Store you first name, last name, age, country, city in your browser localStorage.
 
+localStorage.setItem('firstName', 'Chris')
+localStorage.setItem('lastName', 'Guerrero')
+localStorage.setItem('age', 24)
+localStorage.setItem('country', 'USA')
+localStorage.setItem('city', 'Phoenix')
