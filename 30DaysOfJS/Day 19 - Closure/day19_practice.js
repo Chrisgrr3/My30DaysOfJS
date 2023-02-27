642c3254c57926696edcad73324892d3e1bd8d5e
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-27 12:41:40 -0700

    Created another example of closure

--- 30DaysOfJS/Day 19 - Closure/day19_practice.js
+++ 30DaysOfJS/Day 19 - Closure/day19_practice.js
@@ -14,3 +14,24 @@ console.log(innerFunc())
 console.log(innerFunc())
 
 // In the code above, each time the innerFunc() function is called, the count defined in outerFunction() is incremented.
+
+
+function anotherOuterFunction() {
+    let count = 0;
+    function plusOne() {
+        count++
+        return count
+    }
+    function minusOne() {
+        count--
+        return count
+    }
+    return {
+        plusOne:plusOne(),
+        minusOne:minusOne()
+    }
+}
+const innerFuncs = anotherOuterFunction()
+
+console.log(innerFuncs.plusOne)
+console.log(innerFuncs.minusOne)
