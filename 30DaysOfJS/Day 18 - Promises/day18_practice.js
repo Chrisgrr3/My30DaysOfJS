bdda4e4ffe34d4dfbef1f95efc2f84a5857e36e3
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-26 19:08:10 -0700

    Cleaned up output

--- 30DaysOfJS/Day 18 - Promises/day18_practice.js
+++ 30DaysOfJS/Day 18 - Promises/day18_practice.js
@@ -13,7 +13,7 @@ const callback = (err, result) => {
 	return console.log(result)
 }
 
-doSomething(callback)
+// doSomething(callback)
 
 // Callback function where err is false, returning the else block
 const doSomethingElse = cBack => {
@@ -23,12 +23,12 @@ const doSomethingElse = cBack => {
     }, 2000)
 }
 
-doSomethingElse((err, result) => {
-    if (err) {
-        return console.log(err)
-    }
-    return console.log(result)
-})
+// doSomethingElse((err, result) => {
+//     if (err) {
+//         return console.log(err)
+//     }
+//     return console.log(result)
+// })
 
 // Promise construction syntax
 const promiseSyntax = new Promise((resolve, reject) => {
@@ -37,43 +37,43 @@ const promiseSyntax = new Promise((resolve, reject) => {
 })
 
 // Promise
-const doPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        const skills = ['HTML', 'CSS', 'JS']
-        if(skills.length > 0) {
-            resolve(skills)
-        } else {
-            reject('Something went wrong')
-        }
-    }, 3000)
-})
+// const doPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         const skills = ['HTML', 'CSS', 'JS']
+//         if(skills.length > 0) {
+//             resolve(skills)
+//         } else {
+//             reject('Something went wrong')
+//         }
+//     }, 3000)
+// })
 
-doPromise
-    .then(result => {
-        console.log(result)
-    })
-    .catch(error => console.log(error))
+// doPromise
+//     .then(result => {
+//         console.log(result)
+//     })
+//     .catch(error => console.log(error))
 
 // Now, we'll make an example where the promise is settled with reject:
 
-const rejPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        const skills = ['HTML', 'CSS', 'JS']
-        if(skills.includes('Node')) {
-            resolve('fullstack developer')
-        } else {
-            reject('Something went wrong')
-        }
-    }, 4000)
-})
+// const rejPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         const skills = ['HTML', 'CSS', 'JS']
+//         if(skills.includes('Node')) {
+//             resolve('fullstack developer')
+//         } else {
+//             reject('Something went wrong')
+//         }
+//     }, 4000)
+// })
 
-rejPromise
-    .then(result => {
-        console.log(result)
-    })
-    .catch(error => {
-        console.log(error)
-    })
+// rejPromise
+//     .then(result => {
+//         console.log(result)
+//     })
+//     .catch(error => {
+//         console.log(error)
+//     })
 
 const url = 'https://restcountries.com/v2/all' // countries api
 fetch(url)
