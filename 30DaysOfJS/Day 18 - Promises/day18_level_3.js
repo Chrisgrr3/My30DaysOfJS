9e35460258d1124a5ab3c1b412ac04891c54075a
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-26 23:07:35 -0700

    Began creating fetchData() function

--- 30DaysOfJS/Day 18 - Promises/day18_level_3.js
+++ 30DaysOfJS/Day 18 - Promises/day18_level_3.js
@@ -1,9 +1,19 @@
 const { catsAPI } = require("./day18_level_2");
-const { url } = require('./day18_level_1')
+const countriesAPI = 'https://restcountries.com/v2/all'
 // 1. Read the cats api and find the average weight of cat in metric unit.
 
+const fetchData = async () => {
+    try {
+        const response = await fetch(catsAPI)
+        const cats = await response.json()
+        
+        console.log(catAvg)
+    } catch(err) {
+        console.error(err)
+    }
+}
 
-
+fetchData()
 
 // 2. Read the countries api and find out the 10 largest countries
 
