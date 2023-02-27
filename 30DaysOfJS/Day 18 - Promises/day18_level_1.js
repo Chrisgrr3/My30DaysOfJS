f9a66b81d18a5a311dc49f8d4d0495c7b910017e
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-26 19:58:21 -0700

    Created asynchronous function that reads the name, capital, languages, population, and area of countries in the country API

--- 30DaysOfJS/Day 18 - Promises/day18_level_1.js
+++ 30DaysOfJS/Day 18 - Promises/day18_level_1.js
@@ -1,2 +1,21 @@
 // 1. Read the countries API using fetch and print the name of country, capital, languages, population and area.
 
+const url = 'https://restcountries.com/v2/all'
+
+const fetchData = async () => {
+    try{
+        const response = await fetch(url)
+        const countries = await response.json()
+        let langs = []
+        for(let i = 0; i < countries.length; i++) {
+            for(let j = 0; j < countries[i].languages.length; j++) {
+                langs.push(countries[i].languages[j].name)
+            }
+            console.log('Name:', countries[i].name, '| Capital:', countries[i].capital, '| Languages:', langs, '| Population:', countries[i].population, '| Area:', countries[i].area)
+            langs = []
+        }
+    } catch(err) {
+        console.error(err)
+    }
+}
+fetchData()
