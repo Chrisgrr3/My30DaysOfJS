f9c6a504daf3cdd433f1c37ee1264e5eb7dae7ec
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-27 12:46:05 -0700

    Created function for appending a random value between 0 and 100 to an array

--- 30DaysOfJS/Day 19 - Closure/day19_level_1.js
+++ 30DaysOfJS/Day 19 - Closure/day19_level_1.js
@@ -1,2 +1,16 @@
 // 1. Create a closure which has one inner function
 
+function addRandomToArray() {
+    let arr = [];
+    function addRandom() {
+        arr.push(Math.floor(Math.random() * 101))
+        return arr
+    }
+    return addRandom
+}
+
+const innerRandom = addRandomToArray();
+
+console.log(innerRandom())
+console.log(innerRandom())
+console.log(innerRandom())
