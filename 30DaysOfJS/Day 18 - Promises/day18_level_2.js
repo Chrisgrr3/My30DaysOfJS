31ae2f886a3d9d045b1416c62513830c38d8c69e
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-26 20:06:17 -0700

    Added the name of each cat breed to an array and logged the array to console

--- 30DaysOfJS/Day 18 - Promises/day18_level_2.js
+++ 30DaysOfJS/Day 18 - Promises/day18_level_2.js
@@ -1,2 +1,19 @@
 // 1. Print out all the cat names in to catNames variable.
 
+const catsAPI = 'https://api.thecatapi.com/v1/breeds'
+
+const fetchData = async () => {
+    let catNames = []
+    try {
+        const response = await fetch(catsAPI)
+        const cats = await response.json()
+        for(let i = 0; i < cats.length; i++) {
+            catNames.push(cats[i].name)
+        }
+        console.log(catNames)
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+fetchData()
