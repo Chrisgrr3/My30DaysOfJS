280412b87004d7547b26cfd6a1a7fe82cb4ea18f
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-27 13:13:35 -0700

    Created function with three inner functions that creates a string of random letters

--- 30DaysOfJS/Day 19 - Closure/day19_level_2.js
+++ 30DaysOfJS/Day 19 - Closure/day19_level_2.js
@@ -1,2 +1,44 @@
 // 1. Create a closure which has three inner functions
 
+function doString() {
+    let str = ''
+    let chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_-+={[}]|\\:;"\'?/>.<,`~'
+    function addRandom () {
+        str += chars.charAt(Math.floor(Math.random() * chars.length))
+        return str;
+    }
+    function removeRandom() {
+        str = str.replace(str.charAt(Math.floor(Math.random() * str.length)), '')
+        return str;
+    }
+    function readString() {
+        console.log(str)
+        return str
+    }
+    return {
+        addRandom: addRandom,
+        removeRandom: removeRandom,
+        readString: readString
+    }
+}
+
+const innerFunctions = doString()
+
+innerFunctions.addRandom()
+innerFunctions.addRandom()
+innerFunctions.addRandom()
+innerFunctions.addRandom()
+innerFunctions.addRandom()
+innerFunctions.addRandom()
+innerFunctions.addRandom()
+innerFunctions.addRandom()
+innerFunctions.addRandom()
+innerFunctions.addRandom()
+innerFunctions.readString()
+innerFunctions.removeRandom()
+innerFunctions.removeRandom()
+innerFunctions.removeRandom()
+innerFunctions.removeRandom()
+innerFunctions.removeRandom()
+innerFunctions.readString()
+// console.log(innerFunctions.readString)
