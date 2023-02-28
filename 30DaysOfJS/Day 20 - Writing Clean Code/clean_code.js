540d66138f6db3c8876982a2d39c7212439d4894
Christopher Guerrero <chrisgrr3@gmail.com>
2023-02-28 13:06:06 -0700

    Provided examples of clean code with variables

--- 30DaysOfJS/Day 20 - Writing Clean Code/clean_code.js
+++ 30DaysOfJS/Day 20 - Writing Clean Code/clean_code.js
@@ -1,2 +1,10 @@
 // Modeled after the Airbnb JS Style Guide
 
+// Variables: use let and const, no var
+let firstName = 'Chris'
+let lastName = 'Guerrero'
+let age = 250
+
+const pi = Math.PI
+const gravity = 9.81
+
