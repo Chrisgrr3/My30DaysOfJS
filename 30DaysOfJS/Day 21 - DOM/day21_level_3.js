566828fdc822e9de7e8ff9ea49017135e70d97e4
Christopher Guerrero <chrisgrr3@gmail.com>
2023-03-01 23:58:12 -0700

    Additional formatting

--- 30DaysOfJS/Day 21 - DOM/day21_level_3.js
+++ 30DaysOfJS/Day 21 - DOM/day21_level_3.js
@@ -1,16 +1,16 @@
 // 1. Develop the following application, use the following HTML elements to get started with. You will get the same code on starter folder. Apply all the styles and functionality using JavaScript only.
 
-    // The year color is changing every 1 second
+    // a. The year color is changing every 1 second
 
 
-    // The date and time background color is changing every on seconds
+    // b. The date and time background color is changing every on seconds
 
 
-    // Completed challenge has background green
+    // c. Completed challenge has background green
 
 
-    // Ongoing challenge has background yellow
+    // d. Ongoing challenge has background yellow
 
 
-    // Coming challenges have background red
+    // e. Coming challenges have background red
 
