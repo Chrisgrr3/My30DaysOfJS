04d75864000022ac97f1d939b3c638f223e06e88
Christopher Guerrero <chrisgrr3@gmail.com>
2023-03-02 12:32:06 -0700

    Populated HTML file and provided styling from js file

--- 30DaysOfJS/Day 22 - Manipulating the DOM/day22_level_1.js
+++ 30DaysOfJS/Day 22 - Manipulating the DOM/day22_level_1.js
@@ -1,3 +1,12 @@
+const h1 = document.getElementsByTagName('h1')
+document.addEventListener('DOMContentLoaded', () => {
+    console.log(h1[0])
+    h1[0].style.margin = 'auto'
+    h1[0].style.textAlign = 'center'
+    h1[0].style.fontFamily = "'Courier New', Courier, monospace"
+})
+
+
 // 1. Create a div container on HTML document and create 100 to 100 numbers dynamically and append to the container div.
     
     // Even numbers background is green
