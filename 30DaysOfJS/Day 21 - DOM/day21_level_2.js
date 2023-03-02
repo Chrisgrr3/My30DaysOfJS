03b3b7e485279143b5da0486bc6edcbd7e77c533
Christopher Guerrero <chrisgrr3@gmail.com>
2023-03-01 23:04:40 -0700

    Applied CSS styling to each p tag

--- 30DaysOfJS/Day 21 - DOM/day21_level_2.js
+++ 30DaysOfJS/Day 21 - DOM/day21_level_2.js
@@ -1,6 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     let tags = document.querySelectorAll('p')
-    console.log(tags)
+    for(let i = 0; i < tags.length; i++) {
+        if(i % 2 == 0) {
+            tags[i].style.color = 'green'
+            tags[i].style.background = 'blue'
+            tags[i].style.border = '10px dashed black'
+            tags[i].style.fontSize = '30px'
+            tags[i].style.fontFamily = 'arial'
+        } else {
+            tags[i].style.color = 'orange'
+            tags[i].style.background = 'black'
+            tags[i].style.border = '4px dotted green'
+            tags[i].style.fontSize = '40px'
+            tags[i].style.fontFamily = 'sans-serif'
+        }
+    }
 })
 
 // 1. Change stlye of each paragraph using JavaScript(eg. color, background, border, font-size, font-family)
