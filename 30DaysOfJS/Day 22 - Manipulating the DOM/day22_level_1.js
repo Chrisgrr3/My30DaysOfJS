d334e4eb0dd9f809b859b0f6649b79877d7f53c1
Christopher Guerrero <chrisgrr3@gmail.com>
2023-03-02 12:58:19 -0700

    Created isPrime and isEven functions. Also began creating number divs

--- 30DaysOfJS/Day 22 - Manipulating the DOM/day22_level_1.js
+++ 30DaysOfJS/Day 22 - Manipulating the DOM/day22_level_1.js
@@ -1,9 +1,36 @@
-const h1 = document.getElementsByTagName('h1')
 document.addEventListener('DOMContentLoaded', () => {
+    const h1 = document.getElementsByTagName('h1')
+    const container = document.getElementById("number-container")
     console.log(h1[0])
     h1[0].style.margin = 'auto'
     h1[0].style.textAlign = 'center'
     h1[0].style.fontFamily = "'Courier New', Courier, monospace"
+    let newDiv;
+    const isEven = num => {
+        if(num % 2 == 0) {
+            return true
+        } else {
+            return false
+        }
+    }
+    const isPrime = num => {
+        let result = true;
+        for(let i = 1; i < num; i++) {
+            if(num % i == 0) {
+                result = false
+            }
+            return result
+        }
+    }
+    for(let i = 0; i <= 100; i++) {
+        newDiv = document.createElement('div')
+        newDiv.style.display = 'inline-block'
+        if(isPrime(i)) {
+            newDiv.style.backgroundColor = 'red'
+        }
+        newDiv.innerHTML = i
+        container.appendChild(newDiv)
+    }
 })
 
 
