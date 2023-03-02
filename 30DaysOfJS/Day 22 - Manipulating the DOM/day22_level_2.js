35eea29872a7ee0728a20bc984621944c3895538
Christopher Guerrero <chrisgrr3@gmail.com>
2023-03-02 16:20:57 -0700

    Formatting

--- 30DaysOfJS/Day 22 - Manipulating the DOM/day22_level_2.js
+++ 30DaysOfJS/Day 22 - Manipulating the DOM/day22_level_2.js
@@ -5,12 +5,25 @@ const fetchData = async () => {
     try {
         const response = await fetch(url)
         const countries = await response.json()
+        
         const top = document.getElementById('top')
+        top.style.fontFamily = 'Arial'
+
+
+        const h1 = document.createElement('h1')
+        h1.innerHTML = '<h1>W O R L D&nbsp;&nbsp;&nbsp;C O U N T R I E S&nbsp;&nbsp;&nbsp;L I S T</h1>'
+        h1.style.fontSize = '20px'
+        top.appendChild(h1)
+
         const h2 = document.createElement('h2')
         h2.textContent = `Total Number of Countries: ${countries.length}`
         top.appendChild(h2)
+
         console.log(top)
         top.style.textAlign = 'center'
+
+        
+
     } catch (error) {
         console.error(error)
     }
