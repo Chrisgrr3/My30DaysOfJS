9b93885a2deabd1e1b77bb1f9756aea816cc800c
Christopher Guerrero <chrisgrr3@gmail.com>
2023-03-02 12:11:02 -0700

    Formatting

--- 30DaysOfJS/Day 22 - Manipulating the DOM/day22_level_1.js
+++ 30DaysOfJS/Day 22 - Manipulating the DOM/day22_level_1.js
@@ -1,4 +1,7 @@
-// Create a div container on HTML document and create 100 to 100 numbers dynamically and append to the container div.
-// Even numbers background is green
-// Odd numbers background is yellow
-// Prime numbers background is red
+// 1. Create a div container on HTML document and create 100 to 100 numbers dynamically and append to the container div.
+    
+    // Even numbers background is green
+    
+    // Odd numbers background is yellow
+
+    // Prime numbers background is red
