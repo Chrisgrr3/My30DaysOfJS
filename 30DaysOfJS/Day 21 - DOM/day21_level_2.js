f109809901d80ac463d12a102f55c2e91888918b
Christopher Guerrero <chrisgrr3@gmail.com>
2023-03-01 22:50:23 -0700

    Linked level 2 js file to index.html

--- 30DaysOfJS/Day 21 - DOM/day21_level_2.js
+++ 30DaysOfJS/Day 21 - DOM/day21_level_2.js
@@ -1,4 +1,9 @@
-// 1. Change stye of each paragraph using JavaScript(eg. color, background, border, font-size, font-family)
+document.addEventListener('DOMContentLoaded', () => {
+    let tags = document.querySelectorAll('p')
+    console.log(tags)
+})
+
+// 1. Change stlye of each paragraph using JavaScript(eg. color, background, border, font-size, font-family)
 
 
 // 2. Select all paragraphs and loop through each elements and give the first and third paragraph a color of green, and the second and the fourth paragraph a red color
