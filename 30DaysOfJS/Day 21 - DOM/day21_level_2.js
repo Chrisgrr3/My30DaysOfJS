451c0e4f4d414631cec183910667d8a79c44b3aa
Christopher Guerrero <chrisgrr3@gmail.com>
2023-03-01 22:47:46 -0700

    Formatting

--- 30DaysOfJS/Day 21 - DOM/day21_level_2.js
+++ 30DaysOfJS/Day 21 - DOM/day21_level_2.js
@@ -1,3 +1,7 @@
-// Change stye of each paragraph using JavaScript(eg. color, background, border, font-size, font-family)
-// Select all paragraphs and loop through each elements and give the first and third paragraph a color of green, and the second and the fourth paragraph a red color
-// Set text content, id and class to each paragraph
+// 1. Change stye of each paragraph using JavaScript(eg. color, background, border, font-size, font-family)
+
+
+// 2. Select all paragraphs and loop through each elements and give the first and third paragraph a color of green, and the second and the fourth paragraph a red color
+
+
+// 3. Set text content, id and class to each paragraph
