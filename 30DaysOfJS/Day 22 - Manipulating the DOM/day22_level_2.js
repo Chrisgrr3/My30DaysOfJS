04e82e994e6012b55da659241003e5b3bd38d338
Christopher Guerrero <chrisgrr3@gmail.com>
2023-03-02 16:00:19 -0700

    Fetched countries data from api and made DOM changes

--- 30DaysOfJS/Day 22 - Manipulating the DOM/day22_level_2.js
+++ 30DaysOfJS/Day 22 - Manipulating the DOM/day22_level_2.js
@@ -1,2 +1,19 @@
 // Use the countries array to display all the countries. See the design
 
+const url = 'https://restcountries.com/v3.1/all'
+const fetchData = async () => {
+    try {
+        const response = await fetch(url)
+        const countries = await response.json()
+        const top = document.getElementById('top')
+        const h2 = document.createElement('h2')
+        h2.textContent = `Total Number of Countries: ${countries.length}`
+        top.appendChild(h2)
+        console.log(top)
+        top.style.textAlign = 'center'
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+fetchData()
